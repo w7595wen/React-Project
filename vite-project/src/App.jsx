c489e044cdc738ae323b6d14eeae368e04f5d2ad
w7595wen <wingchee8250@gmail.com>
2025-08-30 17:03:09 +1000

    added boolean cond

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,44 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 const Title = () => {
-  console.log('running Title');             /*give a output message in developer's console */
-  return <div>
-
-  hello world 
-  </div>
+  console.log(
+    "running Title"
+  ); /*give a output message in developer's console */
+  return <div>hello world</div>;
 };
 
-const Description = () => {                 /*start description tag */
-  console.log('running Description');             /*give a output message in developer's console */
+const Description = () => {
+  /*start description tag */
+  console.log(
+    "running Description"
+  ); /*give a output message in developer's console */
   return <div>Enter description here</div>;
 };
 
-const Box = (props) => {                                                 /* tells what type of data this variable is */
-  const {label, bgColor, textColor} = props;                             /* destructure things like label, bgcolor */
-  return <div style={{                                                  /* use built-in style prop on this div */  
-        background: bgColor,
-        color: textColor                                           /* another st of curly brackets is a plain old JS object */
-         }}>
+const Box = (props) => {
+  /* tells what type of data this variable is */
+  const { label, bgColor, textColor, large } =
+    props; /* destructure things like label, bgcolor */
+  return (
+    <div
+      style={{
+        /* use built-in style prop on this div */ background: bgColor,
+        color:
+          textColor /* another st of curly brackets is a plain old JS object */,
+        fontSize: large
+          ? "40px"
+          : "12px" /* another prop is boolean expression (we call large) */,
+      }}
+    >
       {label}
     </div>
+  );
 };
-
 
 function App() {
-  const [count, setCount] = useState(0)
-  return <div>
-    <Box label= 'hello' 
-    bgColor='CornflowerBlue' 
-    textColor= 'Lavender'                                 /*the prop corresponds to what's destructured there */
-    />
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <Box
+        label="hello" /* box 1 */
+        bgColor="CornflowerBlue"
+        textColor="Lavender" /*the prop corresponds to what's destructured there */
+        large={true} /* set to true so apply font size 40 px */
+      />
 
-    <Box label= 'hi' 
-    bgColor='Black' 
-    textColor= 'Lavender'                                 /*the prop corresponds to what's destructured there */
-    />
-  </div>;                                  
-};
+      <Box
+        label="hi" /* box 2*/
+        bgColor="Black"
+        textColor="Lavender" /*the prop corresponds to what's destructured there */
+        large={false} /* set to false so apply font size 12 px */
+      />
+    </div>
+  );
+}
 
 export default App;
