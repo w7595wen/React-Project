60f137511046d5618ac0fa83c0093b6627cfcdc1
w7595wen <wingchee8250@gmail.com>
2025-08-31 22:27:01 +1000

    able to add photos from my github repo if public

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,9 +7,20 @@ import FancyButton from "./FancyButton";
 showing relative path */
 import MyComponent from "./MyComponent.jsx";
 
+const IMAGE_URLS = [
+  "https://github.com/w7595wen/mywebsite/blob/main/images/user.png?raw=true",
+];
+
 function App() {
   const [count, setCount] = useState(0);
-  return <div>to do</div>;
+  return (
+    <div>
+      <img
+        className="object-cover h-[250px] w-[200px] rounded-lg"
+        src="https://github.com/w7595wen/mywebsite/blob/main/images/user.png?raw=true"
+      />
+    </div>
+  );
 }
 
 export default App;
