507a5bafb85d8bb0a891e7cbc01ae0f3b0d346bd
w7595wen <wingchee8250@gmail.com>
2025-09-02 18:10:16 +1000

    add the border and customization settings inside the outermost <div> tag

--- vite-project/src/CatCard.jsx
+++ vite-project/src/CatCard.jsx
@@ -1,8 +1,8 @@
 const CatCard = (props) => {
   const { cat } = props;
   return (
-    <div>
-      <img className="w-80 h-64" src={cat.imageUrl} />
+    <div className="border border-neutral-500 m-6">
+      <img className="w-80 h-64 object-cover" src={cat.imageUrl} />
       <div className="flex text-neutral-500">
         <div className="mr-2 font-bold">Name </div>
         <div>{cat.name}</div>
