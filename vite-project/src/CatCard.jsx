ed376d14cd096c02678ce82c16c8de1711ed75e1
w7595wen <wingchee8250@gmail.com>
2025-09-02 18:02:50 +1000

    added photos and customized photo features

--- vite-project/src/CatCard.jsx
+++ vite-project/src/CatCard.jsx
@@ -2,6 +2,7 @@ const CatCard = (props) => {
   const { cat } = props;
   return (
     <div>
+      <img className="w-80 h-64" src={cat.imageUrl} />
       <div className="flex text-neutral-500">
         <div className="mr-2 font-bold">Name </div>
         <div>{cat.name}</div>
