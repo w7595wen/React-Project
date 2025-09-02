3a5ed3324b7b126f3ec7c4bbc5acf195ba0367cb
w7595wen <wingchee8250@gmail.com>
2025-09-02 19:07:01 +1000

    made text 4 units away from border edge

--- vite-project/src/CatCard.jsx
+++ vite-project/src/CatCard.jsx
@@ -3,14 +3,16 @@ const CatCard = (props) => {
   return (
     <div className="border border-neutral-500 m-6 rounded-lg">
       <img className="w-80 h-64 object-cover" src={cat.imageUrl} />
-      <div className="flex text-neutral-500">
-        <div className="mr-2 font-bold">Name </div>
-        <div>{cat.name}</div>
-      </div>
+      <div className="p-4">
+        <div className="flex text-neutral-500">
+          <div className="mr-2 font-bold">Name </div>
+          <div>{cat.name}</div>
+        </div>
 
-      <div className="flex text-neutral-500">
-        <div className="mr-2 font-bold">Location </div>
-        <div>{cat.location}</div>
+        <div className="flex text-neutral-500">
+          <div className="mr-2 font-bold">Location </div>
+          <div>{cat.location}</div>
+        </div>
       </div>
     </div>
   );
