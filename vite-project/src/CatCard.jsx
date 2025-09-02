92070378cb9cdea4c70d8b67955d04ed6a07f87d
w7595wen <wingchee8250@gmail.com>
2025-09-02 17:17:56 +1000

    added spacing, font color, and font boldness

--- vite-project/src/CatCard.jsx
+++ vite-project/src/CatCard.jsx
@@ -2,8 +2,10 @@ const CatCard = (props) => {
   const { cat } = props;
   return (
     <div>
-      <div>name </div>
-      <div>{cat.name}</div>
+      <div className="flex text-neutral-500">
+        <div className="mr-2 font-bold">name </div>
+        <div>{cat.name}</div>
+      </div>
     </div>
   );
 };
