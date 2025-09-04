e4efb145dd7b4bc860fe366c199404605384aeb6
w7595wen <wingchee8250@gmail.com>
2025-09-04 17:43:48 +1000

    not able to set up {}

--- vite-project/src/QuoteBox1.jsx
+++ vite-project/src/QuoteBox1.jsx
@@ -5,11 +5,11 @@ const QuoteBox1 = (props) => {
   return (
     <div className="flex flex-col m-8">
       <div className="bg-cyan-600 text-cyan-200 p-8 text-lg rounded-t-lg w-[420px]">
-        one
+        text1
       </div>
       <div className="flex flex-col items-center p-4 border rounded-b-lg bg-stone-100 border-stone-300">
-        <div className="text-xl text-cyan-700">two</div>
-        <div className="text-stone-500 ">three</div>
+        <div className="text-xl text-cyan-700">text2</div>
+        <div className="text-stone-500 ">text3</div>
       </div>
     </div>
   );
