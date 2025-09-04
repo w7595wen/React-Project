6c39c35219af9f45e049cd2be3f46f95e8779f16
w7595wen <wingchee8250@gmail.com>
2025-09-04 17:28:48 +1000

    continued styling boxes

--- vite-project/src/QuoteBox1.jsx
+++ vite-project/src/QuoteBox1.jsx
@@ -1,8 +1,16 @@
 const QuoteBox1 = (props) => {
+  const { quote } = props;
+  console.log(quote);
+
   return (
-    <div className="flex flex-col border border-red">
-      <div className="bg-cyan-500">one</div>
-      <div className="bg-slate-100"> two </div>
+    <div className="flex flex-col m-8">
+      <div className="bg-cyan-600 text-cyan-200 p-8 text-lg rounded-t-lg w-[420px]">
+        one
+      </div>
+      <div className="flex flex-col items-center p-4 border rounded-b-lg bg-stone-100 border-stone-300">
+        <div className="text-xl text-cyan-700">two</div>
+        <div className="text-stone-500 ">three</div>
+      </div>
     </div>
   );
 };
