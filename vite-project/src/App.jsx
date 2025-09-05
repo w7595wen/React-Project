35d58e49b9551a94740b5be8662c6e9d0255c731
w7595wen <wingchee8250@gmail.com>
2025-09-05 16:15:36 +1000

    set up new header component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,8 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
+import Header from "./Header.jsx";
+
 function App() {
-  return <div> blank</div>;
+  return (
+    <div>
+      <Header>Header text</Header>
+    </div>
+  );
 }
 
 export default App;
---
+++ vite-project/src/Header.jsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <div className="bg-pink-100">header</div>;
+};
+
+export default Header;
