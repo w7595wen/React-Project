bb66d2f542989984d4338e0ea0549bb5abbd3020
w7595wen <wingchee8250@gmail.com>
2025-09-05 21:15:41 +1000

    made new search bar but not able to put text ont he far left of the search bar by default

--- vite-project/src/Search.jsx
+++ vite-project/src/Search.jsx
@@ -2,8 +2,9 @@ const Search = () => {
   return (
     <div className="flex-1 flex flex-col justify-center items-center bg-violet-100">
       <div className="text-6xl text-pink-400 mb-5">title</div>
-      <div className="text-lg text-pink-400 bg-blue-100 p-2 rounded-full w-80">
-        <i class="fa-solid fa-star"></i> text
+      <div className="flex text-lg text-pink-400 bg-blue-100 p-2 rounded-full w-80">
+        <i className="fa-solid fa-star text-yellow-100 ml-1"></i> what's up
+        <input className="flex-1 bg-blue-200" />
       </div>
     </div>
   );
