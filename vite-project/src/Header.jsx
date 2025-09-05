380e2cb7c793d6bddde656f2f876ef0431883052
w7595wen <wingchee8250@gmail.com>
2025-09-05 16:53:54 +1000

    added fontawesome icon and applied items-center to everything in header

--- vite-project/src/Header.jsx
+++ vite-project/src/Header.jsx
@@ -1,11 +1,13 @@
 const Header = () => {
   return (
     <div className="bg-pink-100 flex justify-between px-4 py-2 text-pink-500">
-      <div className="flex">
+      <div className="flex items-center">
         <div className="mx-4">left1 </div>
         <div className="mx-4">left2</div>
       </div>
-      <div className="mx-4">right</div>
+      <div className="mx-4 items-center">
+        <i class="fa-solid fa-pencil text-xl text-blue-200"></i>
+      </div>
     </div>
   );
 };
