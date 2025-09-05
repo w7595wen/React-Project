d03279c38fabdc536d87bbbf46c58ed0358bf9ef
w7595wen <wingchee8250@gmail.com>
2025-09-05 16:08:28 +1000

    try next design

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,24 +1,9 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import FancyButton from "./FancyButton";
-
-/*Write this line to import component 'MyComponent' from another file, 
-showing relative path */
-import MyComponent from "./MyComponent.jsx";
-
-import QuoteBox1 from "./QuoteBox1.jsx";
-import QuoteBox2 from "./QuoteBox2.jsx";
-import QuoteBox3 from "./QuoteBox3.jsx";
 
 function App() {
-  return (
-    <div className="flex flex-col items-center">
-      <QuoteBox1>quote</QuoteBox1>
-      <QuoteBox2>quote</QuoteBox2>
-      <QuoteBox3>quote</QuoteBox3>
-    </div>
-  );
+  return <div> blank</div>;
 }
 
 export default App;
