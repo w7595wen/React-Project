51ba9a49b205d8c834870f9b22da20b44951fcc0
w7595wen <wingchee8250@gmail.com>
2025-09-05 20:41:18 +1000

    added a component below title

--- vite-project/src/Search.jsx
+++ vite-project/src/Search.jsx
@@ -1,7 +1,10 @@
 const Search = () => {
   return (
-    <div className="flex-1 flex justify-center items-center bg-violet-100">
-      <div className="text-6xl text-pink-400">title</div>
+    <div className="flex-1 flex flex-col justify-center items-center bg-violet-100">
+      <div className="text-6xl text-pink-400 mb-5">title</div>
+      <div className="text-lg text-pink-400 bg-blue-100 p-2 rounded-full w-80">
+        <i class="fa-solid fa-star"></i> text
+      </div>
     </div>
   );
 };
