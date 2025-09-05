c3c4e925330e35e1cfabb99c13585de01101f142
w7595wen <wingchee8250@gmail.com>
2025-09-05 16:46:51 +1000

    customized header pt 2. mainly worked in margin, padding, flex

--- vite-project/src/Header.jsx
+++ vite-project/src/Header.jsx
@@ -1,5 +1,13 @@
 const Header = () => {
-  return <div className="bg-pink-100">header</div>;
+  return (
+    <div className="bg-pink-100 flex justify-between px-4 py-2 text-pink-500">
+      <div className="flex">
+        <div className="mx-4">left1 </div>
+        <div className="mx-4">left2</div>
+      </div>
+      <div className="mx-4">right</div>
+    </div>
+  );
 };
 
 export default Header;
