17262e7d796cfd594295504d0abf9eab21b77a1f
w7595wen <wingchee8250@gmail.com>
2025-09-06 13:57:49 +1000

    turned app.jsx to blank again

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,18 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
-import Header from "./Header.jsx";
-import Search from "./Search.jsx";
-import Footer from "./Footer.jsx";
-
 function App() {
-  return (
-    <div className="h-screen flex flex-col">
-      <Header>Header text</Header>
-      <Search>Search text</Search>
-      <Footer>makes no difference text</Footer>
-    </div>
-  );
+  return <div>blank</div>;
 }
 
 export default App;
