b685501c5e0638b303c0022e335416df9758781e
w7595wen <wingchee8250@gmail.com>
2025-09-06 15:42:10 +1000

    worked on header part 2

--- vite-project/src/Component1.jsx
+++ vite-project/src/Component1.jsx
@@ -1,5 +1,22 @@
 const Component1 = () => {
-  return <div className="bg-pink-100">header</div>;
+  return (
+    <div>
+      <div className="bg-pink-100 py-2 text-slate-100">Dream big.</div>
+      <div className="bg-pink-100 py-4 text-slate-100">
+        The sky's the limit.
+      </div>
+      <div className="flex justify-between">
+        <div className="flex items-center">
+          <i class="fa-solid fa-star text-xl text-slate-100"></i>
+          <i class="fa-solid fa-star text-xl text-slate-100"></i>
+        </div>
+        <div className="flex items-center">
+          <i class="fa-solid fa-star text-xl text-slate-100"></i>
+          <i class="fa-solid fa-star text-xl text-slate-100"></i>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Component1;
