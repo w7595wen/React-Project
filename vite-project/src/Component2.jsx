5c030bf35a725e21cc29c608f20c92fbc545bd84
w7595wen <wingchee8250@gmail.com>
2025-09-06 16:41:45 +1000

    created second component

--- vite-project/src/Component2.jsx
+++ vite-project/src/Component2.jsx
@@ -1,5 +1,16 @@
 const Component2 = () => {
-  return <div className="bg-violet-100 text-slate-100">About me</div>;
+  return (
+    <div>
+      <div className="bg-violet-100 text-slate-100">About me</div>
+      <div className="bg-violet-100 text-slate-100">About me</div>
+      <div className="flex items-center justify-center bg-violet-200">
+        <img
+          className="w-64 rounded-lg"
+          src={"https://static-task-assets.react-formula.com/378212.jpg"}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Component2;
