3999ff864952f7744a09db180536fbf49d2d3d28
w7595wen <wingchee8250@gmail.com>
2025-09-06 15:30:20 +1000

    started component 1

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import Component1 from "./Component1.jsx";
 
 function App() {
-  return <div>blank</div>;
+  return (
+    <div>
+      <Component1>Header text</Component1>
+    </div>
+  );
 }
 
 export default App;
---
+++ vite-project/src/Component1.jsx
@@ -0,0 +1,5 @@
+const Component1 = () => {
+  return <div className="bg-pink-100">header</div>;
+};
+
+export default Component1;
