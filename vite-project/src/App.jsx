16b32839e3cb8ba153481dfaaa87d05863adc300
w7595wen <wingchee8250@gmail.com>
2025-09-07 14:29:10 +1000

    put text side by side horizontally

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -19,7 +19,15 @@ function App() {
   return (
     <div>
       <Component1>Header text</Component1>
+      <div className="bg-violet-100">
+        <button>
+          <i className="fa-regular fa-circle-left text-slate-800 hover:text-yellow-200 mr-2"></i>
+        </button>
+      </div>
       <Component2 display={DISPLAYS[0]} />
+      <button>
+        <i className="fa-regular fa-circle-right text-slate-800 hover:text-yellow-200 mr-2"></i>
+      </button>
     </div>
   );
 }
--- vite-project/src/Component2.jsx
+++ vite-project/src/Component2.jsx
@@ -1,6 +1,13 @@
 const Component2 = (props) => {
   const { display } = props;
-  return <div className="bg-violet-100">hello</div>;
+  return (
+    <div className="bg-violet-100">
+      <div className="flex flex-row items-center justify-center">
+        <div className="mr-4 font-bold">Static text</div>
+        <div>{display.text1}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Component2;
