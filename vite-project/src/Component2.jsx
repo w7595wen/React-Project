d58a1d1f158d50f8e2b12b29101ddd453d251201
w7595wen <wingchee8250@gmail.com>
2025-09-09 16:01:49 +1000

    trying how to best align texts in component 2

--- vite-project/src/Component2.jsx
+++ vite-project/src/Component2.jsx
@@ -1,14 +1,14 @@
 const Component2 = (props) => {
   const { display } = props;
   return (
-    <div className="bg-[#012746] p-4">
+    <div className="bg-[#012746] p-4 text-md  text-slate-100 ">
       <div className="flex flex-row items-center justify-center">
-        <div className="mr-4 font-bold text-slate-100">{display.q1}</div>
-        <div className="text-slate-100">{display.a1}</div>
+        <div className="mr-4 font-bold w-60 mr-2">{display.q1}</div>
+        <div>{display.a1}</div>
       </div>
       <div className="flex flex-row items-center justify-center">
-        <div className="mr-4 font-bold text-slate-100">{display.q2}</div>
-        <div className="text-slate-100">{display.a2}</div>
+        <div className="mr-4 font-bold w-60 mr-2">{display.q2}</div>
+        <div>{display.a2}</div>
       </div>
       <div className="flex items-center justify-center">
         <img className="w-80 h-64" src={display.imageUrl} />
