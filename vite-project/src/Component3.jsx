e2db6eb9562c7259d5f1fbe325f75ebc6241b0f5
w7595wen <wingchee8250@gmail.com>
2025-09-11 18:45:44 +1000

    re-sized photos

--- vite-project/src/Component3.jsx
+++ vite-project/src/Component3.jsx
@@ -2,16 +2,16 @@ const Component3 = (props) => {
   return (
     <div className="bg-[#012746]">
       <img
-        className="object-cover h-[450px] w-[1400px]"
-        src="https://github.com/w87895wen/React-Project/blob/main/vite-project/images/pic%20below%20component%204(800%20x%20300%20px).png?raw=true"
+        className="object-cover h-[350px] w-[1400px]"
+        src="https://github.com/w87895wen/React-Project/blob/main/vite-project/images/paper_pt1(800%20x%20300%20px).png?raw=true"
       />
       <img
         className="object-cover h-[200px] w-[1400px]"
         src="https://github.com/w87895wen/React-Project/blob/main/vite-project/images/stars_v4(800%20x%20300%20px).png?raw=true"
       />
       <img
-        className="object-cover h-[450px] w-[1400px]"
-        src="https://github.com/w87895wen/React-Project/blob/main/vite-project/images/pic%20below%20component%204(800%20x%20300%20px).png?raw=true"
+        className="object-cover h-[350px] w-[1400px]"
+        src="https://github.com/w87895wen/React-Project/blob/main/vite-project/images/paper_pt2(800%20x%20300%20px).png?raw=true"
       />
     </div>
   );
